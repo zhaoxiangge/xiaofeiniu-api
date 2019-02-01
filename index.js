93fe586509dc41657ced5b10b02b9cc2a8d5c78d
zhaoxiangge <577584496@qq.com>
2019-02-01 16:59:41 +0800

    提交一次

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 *小肥牛扫码点餐项目API子系统
 */
 //const PORT = 8090; 
-const PORT = 5050; 
+const PORT = 5050;    
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
